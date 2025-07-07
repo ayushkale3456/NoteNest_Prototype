65d3c6b9a9f237c7a363afe64c54d6cb83d31008
ayushkale3456 <ayushkale35@gmail.com>
2025-07-07 12:19:20 +0530

    modified NoteController, application.properties and commented out WebConfig

--- notenest2.0/src/main/java/com/dimsum/notenest20/WebConfig.java
+++ notenest2.0/src/main/java/com/dimsum/notenest20/WebConfig.java
@@ -1,18 +1,18 @@
 package com.dimsum.notenest20;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer
-            .favorParameter(false)
-            .ignoreAcceptHeader(false)
-            .defaultContentType(MediaType.APPLICATION_JSON)
-            .mediaType("multipart", MediaType.MULTIPART_FORM_DATA);
-    }
-}
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.MediaType;
+//import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//    @Override
+//    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+//        configurer
+//            .favorParameter(false)
+//            .ignoreAcceptHeader(false)
+//            .defaultContentType(MediaType.APPLICATION_JSON)
+//            .mediaType("multipart", MediaType.MULTIPART_FORM_DATA);
+//    }
+//}
