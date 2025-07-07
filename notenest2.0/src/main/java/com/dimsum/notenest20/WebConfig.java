c58278ec428cf01813c2876b94db3b4785837248
ayushkale3456 <ayushkale35@gmail.com>
2025-07-07 12:02:51 +0530

    modified WebConfig and NoteController

--- notenest2.0/src/main/java/com/dimsum/notenest20/WebConfig.java
+++ notenest2.0/src/main/java/com/dimsum/notenest20/WebConfig.java
@@ -1,14 +1,18 @@
 package com.dimsum.notenest20;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/files/**")
-            .addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/");
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer
+            .favorParameter(false)
+            .ignoreAcceptHeader(false)
+            .defaultContentType(MediaType.APPLICATION_JSON)
+            .mediaType("multipart", MediaType.MULTIPART_FORM_DATA);
     }
 }
