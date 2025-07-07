f57fe055af0f09bdafba66e90cf1985b80c3f7e0
ayushkale3456 <ayushkale35@gmail.com>
2025-07-07 20:17:16 +0530

    modified WebConfig.java

--- notenest2.0/src/main/java/com/dimsum/notenest20/WebConfig.java
+++ notenest2.0/src/main/java/com/dimsum/notenest20/WebConfig.java
@@ -5,8 +5,14 @@ import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import jakarta.annotation.PostConstruct;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+	@PostConstruct
+    public void init() {
+        System.out.println("WebConfig is being initialized."); // Or use a proper logger
+    }
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 	    configurer
