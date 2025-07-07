135b06a93fbf25d1eb25daa6b196f586491c23f4
ayushkale3456 <ayushkale35@gmail.com>
2025-07-07 13:00:39 +0530

    modified WebConfig

--- notenest2.0/src/main/java/com/dimsum/notenest20/WebConfig.java
+++ notenest2.0/src/main/java/com/dimsum/notenest20/WebConfig.java
@@ -7,12 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer
-            .favorParameter(false)
-            .ignoreAcceptHeader(false)
-            .defaultContentType(MediaType.APPLICATION_JSON)
-            .mediaType("multipart", MediaType.MULTIPART_FORM_DATA);
-    }
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+	    configurer
+	        .favorParameter(false)
+	        .ignoreAcceptHeader(true)
+	        .defaultContentType(MediaType.MULTIPART_FORM_DATA); // allow multipart by default
+	}
+
 }
